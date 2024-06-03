9e9fe6f3732fc3775da408f74b70b1c04d2c0d5a
THANETNove <novel2020manga@gmail.com>
2024-06-03 11:32:24 +0700

    add

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,7 +1,4 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
+
 
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
@@ -23,4 +20,11 @@ export const Colors = {
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
   },
+  primary: "#662D91",
+  white:"#FFF",
+  red:"#DB100F",
+  green:"#009244",
+  blue:"#0171BC",
+  yellow:"#EACE09"
+  
 };
