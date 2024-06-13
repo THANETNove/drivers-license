d5590ea5d9d3438c9409ff3e9915d152b346935f
THANETNove <novel2020manga@gmail.com>
2024-06-13 19:34:28 +0700

    add

--- app/src/driverLicense/HomeDriver.js
+++ app/src/driverLicense/HomeDriver.js
@@ -2,8 +2,18 @@ import React from 'react';
 import { Dimensions, View, Image, Pressable, StyleSheet } from 'react-native';
 import Carousel from 'react-native-reanimated-carousel';
 
+import button_1 from "../../../assets/images/home/button_1.png";
+import button_2 from "../../../assets/images/home/button_2.png";
+import button_3 from "../../../assets/images/home/button_3.png";
+import button_4 from "../../../assets/images/home/button_4.png";
+import button_5 from "../../../assets/images/home/button_5.png";
+import button_6 from "../../../assets/images/home/button_6.png";
+
+
+
 const HomeDriver = ({ navigation }) => {
   const width = Dimensions.get('window').width;
+  const height = Dimensions.get('window').height;
 
   const images = [
     require('../../../assets/images/home/home_1.png'),
@@ -20,7 +30,7 @@ const HomeDriver = ({ navigation }) => {
         loop
         width={width}
         height={"100%"}
-        autoPlay={false} // Disable autoplay to control manually
+        autoPlay={true} // Disable autoplay to control manually
         data={images}
         scrollAnimationDuration={1000}
         renderItem={({ item }) => (
@@ -30,13 +40,53 @@ const HomeDriver = ({ navigation }) => {
               justifyContent: 'center',
             }}
           >
-
             <Image
               source={item}
               style={{ width: '100%', height: '100%' }}
               resizeMode="stretch"
             />
-            <View style={styles.boxNumber} />
+            <View style={styles.boxNumber_1} >
+              <Image
+                source={button_1}
+                style={{ width: '100%', height: '100%' }}
+                resizeMode="stretch"
+              />
+            </View>
+            <View style={styles.boxNumber_2} >
+              <Image
+                source={button_2}
+                style={{ width: '100%', height: '100%' }}
+                resizeMode="stretch"
+              />
+            </View>
+            <View style={styles.boxNumber_3} >
+              <Image
+                source={button_3}
+                style={{ width: '100%', height: '100%' }}
+                resizeMode="stretch"
+              />
+            </View>
+            <View style={styles.boxNumber_4} >
+              <Image
+                source={button_4}
+                style={{ width: '100%', height: '100%' }}
+                resizeMode="stretch"
+              />
+            </View>
+            <View style={styles.boxNumber_5} >
+              <Image
+                source={button_5}
+                style={{ width: '100%', height: '100%' }}
+                resizeMode="stretch"
+              />
+            </View>
+            <View style={styles.boxNumber_6} >
+              <Image
+                source={button_6}
+                style={{ width: '100%', height: '100%' }}
+                resizeMode="stretch"
+              />
+            </View>
           </View>
         )}
       />
@@ -45,14 +95,58 @@ const HomeDriver = ({ navigation }) => {
 }
 
 const styles = StyleSheet.create({
-  boxNumber: {
-    width: 60,
-    height: 60,
-    backgroundColor: "red",
+  boxNumber_1: {
+    width: 50,
+    height: 50,
     position: "absolute",
-    marginLeft: "25%",
+    marginLeft: "27%",
     top: 0,
-    marginTop: "30%"
+    marginTop: "35%"
+  },
+  boxNumber_2: {
+    width: 50,
+    height: 50,
+    position: "absolute",
+    marginRight: "18%",
+    top: 0,
+    right: 0,
+    marginTop: "37%"
+  },
+  boxNumber_3: {
+    width: 50,
+    height: 50,
+    position: "absolute",
+    marginRight: "0%",
+    top: 0,
+    right: 0,
+    marginTop: "59%"
+  },
+  boxNumber_4: {
+    width: 50,
+    height: 50,
+    position: "absolute",
+    marginRight: "35%",
+    top: 0,
+    right: 0,
+    marginTop: "78%"
+  },
+  boxNumber_5: {
+    width: 50,
+    height: 50,
+    position: "absolute",
+    marginRight: "43%",
+    top: 0,
+    right: 0,
+    marginTop: "102%"
+  },
+  boxNumber_6: {
+    width: 50,
+    height: 50,
+    position: "absolute",
+    marginRight: "60%",
+    top: 0,
+    right: 0,
+    marginTop: "130%"
   },
 });
 
