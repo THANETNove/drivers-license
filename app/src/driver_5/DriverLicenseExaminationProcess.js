bb867a3cabdab645d7e6c8073b377f6ef1912ae5
THANETNove <novel2020manga@gmail.com>
2024-06-30 18:54:09 +0700

    driver_5 เสร็จ

--- app/src/driver_5/DriverLicenseExaminationProcess.js
+++ app/src/driver_5/DriverLicenseExaminationProcess.js
@@ -4,8 +4,8 @@ import { View, Text, ScrollView, StyleSheet, Pressable, Dimensions, Image } from
 import { LinearGradient } from 'expo-linear-gradient';
 import { useNavigation, useRoute } from '@react-navigation/native'; // ใช้ useNavigation จาก react-navigation
 import ScreenContainer from "../NavigationProvider"; // ปรับเส้นทางตามที่ถูกต้อง
-import Artboard5_63 from "../../../assets/images/coverImg/Artboard5_63.png";
-import Artboard5_64 from "../../../assets/images/coverImg/Artboard5_64.png";
+import Artboard15 from "../../../assets/images/coverImg/Artboard15-0.jpg";
+import Artboard16 from "../../../assets/images/coverImg/Artboard16-0.jpg";
 
 
 const DriverLicenseExaminationProcess = () => {
@@ -14,10 +14,27 @@ const DriverLicenseExaminationProcess = () => {
     const { id } = route.params; // รับพารามิเตอร์ id
     console.log("id", id);
     return (
-        <ScreenContainer>
-            <Text>DriverLicenseExaminationProcess</Text>
+        <ScreenContainer >
+            <View style={styles.container}>
+                <Image
+                    source={id && id == 1 ? Artboard15 : Artboard16} // ใช้ source แทน src
+                    style={styles.imageArt5}
+                />
+            </View>
         </ScreenContainer>
 
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+
+    },
+    imageArt5: {
+        width: "100%",
+        height: "100%",
+        resizeMode: "stretch"
+    },
+});
 export default DriverLicenseExaminationProcess;
