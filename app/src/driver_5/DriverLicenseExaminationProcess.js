16e6051c56e87e5165482eeecf49f4c126c947a3
THANETNove <novel2020manga@gmail.com>
2024-06-30 19:46:18 +0700

    boxVideo

--- app/src/driver_5/DriverLicenseExaminationProcess.js
+++ app/src/driver_5/DriverLicenseExaminationProcess.js
@@ -4,8 +4,8 @@ import { View, Text, ScrollView, StyleSheet, Pressable, Dimensions, Image } from
 import { LinearGradient } from 'expo-linear-gradient';
 import { useNavigation, useRoute } from '@react-navigation/native'; // ใช้ useNavigation จาก react-navigation
 import ScreenContainer from "../NavigationProvider"; // ปรับเส้นทางตามที่ถูกต้อง
-import Artboard15 from "../../../assets/images/coverImg/Artboard15-0.jpg";
-import Artboard16 from "../../../assets/images/coverImg/Artboard16-0.jpg";
+import Artboard15 from "../../../assets/images/coverImg/Artboard57-100.jpg";
+import Artboard16 from "../../../assets/images/coverImg/Artboard59-100.jpg";
 
 
 const DriverLicenseExaminationProcess = () => {
@@ -20,21 +20,33 @@ const DriverLicenseExaminationProcess = () => {
                     source={id && id == 1 ? Artboard15 : Artboard16} // ใช้ source แทน src
                     style={styles.imageArt5}
                 />
+                <View style={styles.boxVideo}></View>
             </View>
         </ScreenContainer>
 
     )
 }
+const width = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-
+        alignItems: "center"
     },
     imageArt5: {
         width: "100%",
         height: "100%",
-        resizeMode: "stretch"
+        resizeMode: "stretch",
+        zIndex: 0
     },
+    boxVideo: {
+        width: width - 64,
+        height: 200,
+        borderColor: Colors.primary2,
+        borderWidth: 1,
+        zIndex: 1,
+        position: "absolute",
+        bottom: 120
+    }
 });
 export default DriverLicenseExaminationProcess;
