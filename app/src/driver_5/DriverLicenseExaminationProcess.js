d54e401efe49d20ed769bafca49f2642c9610a33
THANETNove <novel2020manga@gmail.com>
2024-07-02 13:24:20 +0700

    YoutubePlayer

--- app/src/driver_5/DriverLicenseExaminationProcess.js
+++ app/src/driver_5/DriverLicenseExaminationProcess.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import { Colors } from "@/constants/Colors";
-import { View, Text, ScrollView, StyleSheet, Pressable, Dimensions, Image } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Pressable, Dimensions, Image, Button } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useNavigation, useRoute } from '@react-navigation/native'; // ใช้ useNavigation จาก react-navigation
 import ScreenContainer from "../NavigationProvider"; // ปรับเส้นทางตามที่ถูกต้อง
 import Artboard15 from "../../../assets/images/coverImg/Artboard57-100.jpg";
 import Artboard16 from "../../../assets/images/coverImg/Artboard59-100.jpg";
+import { Video, ResizeMode } from 'expo-av';
+import YoutubePlayer from 'react-native-youtube-iframe';
 
 
 const DriverLicenseExaminationProcess = () => {
 
     const route = useRoute();
     const { id } = route.params; // รับพารามิเตอร์ id
-    console.log("id", id);
+
+    const video = React.useRef(null);
+    const [status, setStatus] = React.useState({});
+
     return (
         <ScreenContainer >
             <View style={styles.container}>
@@ -20,7 +25,26 @@ const DriverLicenseExaminationProcess = () => {
                     source={id && id == 1 ? Artboard15 : Artboard16} // ใช้ source แทน src
                     style={styles.imageArt5}
                 />
-                <View style={styles.boxVideo}></View>
+                <View style={styles.boxVideo}>
+                    {/*   <Video
+                        ref={video}
+                        style={styles.video}
+                        source={{
+                            uri: 'http://127.0.0.1/project/DriversLicenseImage/video/practiceMotorcycle.mp4',
+                        }}
+                        useNativeControls
+                        resizeMode={ResizeMode.CONTAIN}
+                        isLooping
+                        onPlaybackStatusUpdate={status => setStatus(() => status)}
+                    /> */}
+                    <YoutubePlayer
+                        height={250}
+                        width={width - 64}
+                        videoId={id == 1 ? "DRumUD6VSqU" : "zWlALDmoia4"} // Replace with your YouTube video ID
+                        play={true} // Autoplay the video
+
+                    />
+                </View>
             </View>
         </ScreenContainer>
 
@@ -47,6 +71,11 @@ const styles = StyleSheet.create({
         zIndex: 1,
         position: "absolute",
         bottom: "18%"
+    },
+    video: {
+        width: "100%",
+        height: "100%",
+        zIndex: 2,
     }
 });
 export default DriverLicenseExaminationProcess;
