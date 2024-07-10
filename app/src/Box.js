95aa0dcda1bc6cc3a8359ce0278aa63156318f8c
THANETNove <novel2020manga@gmail.com>
2024-07-10 15:45:10 +0700

    เพิ่ม ca-app-pub

--- app/src/Box.js
+++ app/src/Box.js
@@ -1,10 +1,10 @@
 // Box.js
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Platform } from 'react-native';
 import { Colors } from "@/constants/Colors";
 import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
 
-const adUnitId = __DEV__ ? TestIds.BANNER : 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy';
+const adUnitId = __DEV__ ? TestIds.BANNER : Platform.OS ? 'ca-app-pub-6813229715486122/2248366452' : 'ca-app-pub-6813229715486122/8953375601';
 
 const Box = () => {
     return <View style={styles.box} >
