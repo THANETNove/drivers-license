03cc1695c1739a4170c35bbbfb19632527fcbe70
THANETNove <novel2020manga@gmail.com>
2024-07-10 16:01:23 +0700

    add code

--- app/src/Box.js
+++ app/src/Box.js
@@ -1,5 +1,5 @@
 // Box.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, Text, Platform } from 'react-native';
 import { Colors } from "@/constants/Colors";
 import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
@@ -7,11 +7,32 @@ import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads'
 const adUnitId = __DEV__ ? TestIds.BANNER : Platform.OS ? 'ca-app-pub-6813229715486122/2248366452' : 'ca-app-pub-6813229715486122/8953375601';
 
 const Box = () => {
+
+    const [isAdLoaded, setIsAdLoaded] = useState(true);
+
+    useEffect(() => {
+        const loadBannerAd = async () => {
+            // Load the banner ad
+            await BannerAd.requestAd(adUnitId);
+            setIsAdLoaded(true);
+        };
+
+        loadBannerAd();
+
+        return () => {
+            // Clean up when unmounting the component
+            setIsAdLoaded(false);
+        };
+    }, []);
+
+
     return <View style={styles.box} >
-        <BannerAd
-            unitId={adUnitId}
-            size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
-        />
+        {isAdLoaded && (
+            <BannerAd
+                unitId={adUnitId}
+                size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+            />
+        )}
     </View>;
 };
 
